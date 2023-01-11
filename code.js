99fac01de072650524eac4c38a48c8809dc8d06e
SinixND <freak105@gmx.de>
2023-01-11 18:12:33 +0100

    revert to pre-messing around

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
+/*
+//Initializing
+var i = 0;
+var itemClasses = [];
+var icons = [];
 
+//ids
+itemClasses = ['main-weapon', 'sidearm', 'mask', 'backpack', 'chest', 'gloves', 'holster', 'kneepads'];
+
+//function
+function setBackgroundIcons() {
+    for (i < itemClasses.length -1) {
+        var element document.getElementByClassName(i);
+        element.style.background = var(--cbg75), "url("icons/${i}.png")";
+    }
+}
+
+//apply function
+window.onload = setBackgroundIcons();
+*/
