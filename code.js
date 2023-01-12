4df73e9308ff693ef040ece9f821568ae6210daf
676d <a.schaumberger@oth-aw.de>
2023-01-12 23:31:53 +0100

    update js, not working

--- code.js
+++ code.js
@@ -1,14 +1,49 @@
-function createListFromCSV(){
-    return new Promise((resolve, reject) => {
-        const wholeCSV = [];
-        fs.createReadStream('../csv/countrycodes.csv')
-            .on('error' , reject)
-            .pipe(csvParser())
-            .on('data', (row) => {
-                wholeCSV.push(row);
-             })
-            .on('end', ()=> {
-                resolve(wholeCSV);
-            });
-    })
-}
+/*
+function f_return(arg) {
+    //do something like displaying
+};
+
+let myPromise = new Promise(function(myResolve, myReject) {
+    
+    // "Producing Code" (May take some time)
+
+    myResolve(console.log("promise: resolve")); // when successful
+    myReject(console.log("promise: reject"));  // when error
+});
+
+// "Consuming Code" (Must wait for a fulfilled Promise)
+myPromise.then(
+    function(value) {console.log(value)},
+    function(error) {console.log(error)}
+);
+*/
+
+function csvParser(filename) {// incl. *.filetyp
+    //return new Promise((resolve, reject) => {
+
+    //})
+    // initialize
+    const fs = require("fs");
+    const readline = require("readline");
+
+    const rfs = fs.createReadStream(`./csv/${filename}`);
+    const reader = readline.createInterface({ input: rfs });
+
+    let arr = [];
+    reader.on("line", row => {
+        // split row string in array and push
+        arr.push(row.split(","));
+        console.log("1: pushed: " + row.split(","));
+    });
+
+    reader.on("close", () => {
+        console.log("2: onClose: ");
+        console.log(arr);
+    });
+    console.log("return arr: " + arr);
+
+    return arr;
+};
+
+let parsedCSV = csvParser('data.csv');
+console.log("finalarr: " + parsedCSV);
