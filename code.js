680ac40db25726fdbee9d7b558f3db52f8d90aec
SinixND <freak105@gmx.de>
2023-01-12 23:43:24 +0100

    update

--- code.js
+++ code.js
@@ -1,4 +1,36 @@
 /*
+//==============
+    //
+// Traditional anonymous function
+(function (a) {
+  return a + 100;
+});
+
+// 1. Remove the word "function" and place arrow between the argument and opening body bracket
+(a) => {
+  return a + 100;
+};
+
+// 2. Remove the body braces and word "return" — the return is implied.
+(a) => a + 100;
+
+// 3. Remove the parameter parentheses
+a => a + 100;
+
+//==============
+//==============
+
+// Traditional Function
+function bob(a) {
+  return a + 100;
+}
+
+// Arrow Function
+const bob2 = (a) => a + 100;
+
+//==============
+*/
+/*
 function f_return(arg) {
     //do something like displaying
 };
