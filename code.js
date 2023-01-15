c831cb343e019561ecef96e6c08346dbd4c5294d
SinixND <freak105@gmx.de>
2023-01-15 20:18:59 +0100

    update json parser

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
 //import Import from './data/weapons.json';
 const Const = require('./data/weapons.json');
-const data = JSON.parse(Const);
+//const data = JSON.parse(Const);
 //console.log(data.attrib);
