6540f7129e2faa77b5037a43a07c8a4464209e58
SinixND <freak105@gmx.de>
2023-01-16 15:11:46 +0100

    update

--- code.js
+++ code.js
@@ -9,3 +9,12 @@ console.log(weapons.mmr);
 
 console.log("\nLevel 3: ");
 console.log(weapons.mmr._1886.damage);
+
+
+function showPopupSpecialisation() {
+    document.getElementById("popupSpecialisation").style.display = "block";
+}
+
+function closePopupSpecialisation() {
+  document.getElementById("popupSpecialisation").style.display = "none";
+} 
