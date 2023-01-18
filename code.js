f8f47b880473ea49d8e5b57dfe17ec9c1415fe89
SinixND <77335684+SinixND@users.noreply.github.com>
2023-01-18 18:36:40 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,20 +1,23 @@
-//import Import from './data/weapons.json';
+// set content background icons per class/itemtype
+/*
+document.addEventListener('DOMContentLoaded', function () {
+    let items = ["main-weapon", "sidearm", "mask", "backpack", "chest", "gloves", "holster", "kneepads"];
+    for (let i = 0; i < items.size; i++) {
+        document.getElementsByClassName(`.content.${items[i]}`).style.background = linear-gradient(var(--cbg75), var(--cbg75)), url(`icons/${items[i]}.png`) center/contain no-repeat;
+    };
+}, false);
+*/
+
+// import Import from './data/weapons.json';
 const weapons = require("./data/weapons.json");
-//const data = JSON.parse(Const);
-console.log("\nLevel 1: ");
-console.log(weapons);
-
-console.log("\nLevel 2: ");
-console.log(weapons.mmr);
-
-console.log("\nLevel 3: ");
 console.log(weapons.mmr._1886.damage);
 
-
+// show specialisation selection popup
 function showPopupSpecialisation() {
     document.getElementById("popupSpecialisation").style.display = "block";
 }
 
+// hide specialisation selection popup
 function closePopupSpecialisation() {
   document.getElementById("popupSpecialisation").style.display = "none";
 } 
