2e96ba19108d87a444b53de888fec8a7f20076a2
676d <a.schaumberger@oth-aw.de>
2023-01-18 20:49:53 +0100

    automated icons working

--- code.js
+++ code.js
@@ -1,21 +1,15 @@
 // set content background icons per class/itemtype
 document.addEventListener('DOMContentLoaded', function () {
     let items = ["main-weapon", "sidearm", "mask", "backpack", "chest", "gloves", "holster", "kneepads"];
-    for (let i = 0; i < items.size; i++) {
-        console.log(`Working on item ${items[i]}`);
-        document.getElementsByClassName(`.content.${items[i]}`).style.background = linear-gradient(var(--cbg75), var(--cbg75)), url(`icons/${items[i]}.png`) center/contain no-repeat;
+    for (let i = 0; i < items.length; i++) {
+        let file = `./icons/${items[i]}.png`;
+        let value = `linear-gradient(var(--cbg75), var(--cbg75)), url(${file}) center/contain no-repeat`;
+        let element = document.getElementsByClassName(`content ${items[i]}`);
+        for (let j = 0; j < element.length; j++) {
+            element[j].style.background = value;
+        }
     };
 }, false);
-
-/*
-or try this one:
-const boxes = document.querySelectorAll('.box');
-
-boxes.forEach(box => {
-  box.style.backgroundColor = 'purple';
-  box.style.width = '300px';
-});
-*/
 
 // import Import from './data/weapons.json';
 const weapons = require("./data/weapons.json");
@@ -28,5 +22,5 @@ function showPopupSpecialisation() {
 
 // hide specialisation selection popup
 function closePopupSpecialisation() {
-  document.getElementById("popupSpecialisation").style.display = "none";
+    document.getElementById("popupSpecialisation").style.display = "none";
 } 
