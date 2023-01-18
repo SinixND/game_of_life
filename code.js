a7eccff24a6fa2f784697889a2767c6b124fa7dd
SinixND <77335684+SinixND@users.noreply.github.com>
2023-01-18 18:46:14 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,20 @@
 // set content background icons per class/itemtype
-/*
 document.addEventListener('DOMContentLoaded', function () {
     let items = ["main-weapon", "sidearm", "mask", "backpack", "chest", "gloves", "holster", "kneepads"];
     for (let i = 0; i < items.size; i++) {
+        console.log(`Working on item ${items[i]}`);
         document.getElementsByClassName(`.content.${items[i]}`).style.background = linear-gradient(var(--cbg75), var(--cbg75)), url(`icons/${items[i]}.png`) center/contain no-repeat;
     };
 }, false);
+
+/*
+or try this one:
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box => {
+  box.style.backgroundColor = 'purple';
+  box.style.width = '300px';
+});
 */
 
 // import Import from './data/weapons.json';
