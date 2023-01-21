8b1f507a762ecc2069464a4f9ab83f119bd7067b
676d <a.schaumberger@oth-aw.de>
2023-01-21 03:41:43 +0100

    update

--- code.js
+++ code.js
@@ -9,23 +9,21 @@ document.addEventListener('DOMContentLoaded', function () {
             element[j].style.background = value;
         }
     };
+    console.log("debug");
 }, false);
 
-document.addEventListener('DOMContentLoaded', function () {
-    // import data
-    //const masks = require("./data/masks.json");
-    let element = document.getElementsByClassName("item-name");
-  console.log(element);
-    element.innerHTML += "test";
-    //console.log(file.name.key);
-    //document.getElementsByClassName(`item-name`).innerHTML+=`${masks.Vile.rarity}`;
-    //try:
-    //console.log(file[name].key);
-    //document.getElementsByClassName(`item-name`).innerHTML+=`${masks[Vile].rarity}`;
-    //and:
-    //console.log(file[name][key]);
-    //document.getElementsByClassName(`item-name`).innerHTML+=`${masks[Vile][rarity]}`;
-}, false);
+// import data
+const masks = require("./data/masks.json");
+//let element = document.getElementsByClassName(`item-name`).innerHTML;
+//console.log(element);
+//console.log(file.name.key);
+//document.getElementsByClassName(`item-name`).innerHTML+=`${masks.Vile.rarity}`;
+//try:
+//console.log(file['name'].key);
+//document.getElementsByClassName(`item-name`).innerHTML+=`${masks[Vile].rarity}`;
+//and:
+//console.log(file[name][key]);
+//document.getElementsByClassName(`item-name`).innerHTML+=`${masks[Vile][rarity]}`;
 
 // show selection popup
 function showPopup(Class) {
