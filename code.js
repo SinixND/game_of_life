eec644979cb0c8ecff41572f6cf96ee35982632f
SinixND <freak105@gmx.de>
2023-01-23 12:42:53 +0100

    try imports further

--- code.js
+++ code.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // import data
 //const masks = require("./data/masks.json");
-import masks from "./data/masks.json";
+import * as masks from "./data/masks.js";
 //(async () => { 
 //    const masks = await import('./data/masks.js');
 //    console.log(masks);
