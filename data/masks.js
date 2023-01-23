b8cc94276d9b62127e535b9e2af8cbd836052844
SinixND <77335684+SinixND@users.noreply.github.com>
2023-01-23 13:37:05 +0100

    Update masks.js

--- data/masks.js
+++ data/masks.js
@@ -1,20 +1,20 @@
 export const masks = {
   "Vile": {
-    "rarity": "Exotic",
-    "type": "Exotic",
-    "talentName": "Toxic Delivery",
-    "talentText": "Status effects also apply a damage over time debuff for 10s.\n Total damage dealt is equal to 50% of your concussion grenade damage and increased by your status effect attributes.",
+    rarity: "Exotic",
+    type: "Exotic",
+    talentName: "Toxic Delivery",
+    talentText: "Status effects also apply a damage over time debuff for 10s.\n Total damage dealt is equal to 50% of your concussion grenade damage and increased by your status effect attributes.",
   },
   "Nightwatcher": {
-    "rarity": "Named",
-    "type": "Gila Guard"
+    rarity: "Named",
+    type: "Gila Guard"
   },
   "Aces & Eights": {
-    "rarity": "Set",
-    "type": "Aces & Eights"
+    rarity: "Set",
+    type: "Aces & Eights"
   },
   "5.11 Tactical": {
-    "rarity": "High-End",
-    "type": "5.11 Tactical"
+    rarity: "High-End",
+    type": "5.11 Tactical"
   }
 }
