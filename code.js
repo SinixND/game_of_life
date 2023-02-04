9a0730d52067866130feaf5842fc8cb579bae027
676d <a.schaumberger@oth-aw.de>
2023-02-04 22:45:54 +0100

    set icons

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 //============================
 // IMPORT DATA
+import {set} from "./data/sets.js";
 import {mask} from "./data/masks.js";
 
 //============================
@@ -8,10 +9,8 @@ document.addEventListener('DOMContentLoaded', function () {
   //============================
   // DEBUG
   //console.log(mask.Vile.talentName);
-  alert( 'Res: (W)' + screen.width + 'x(H)' + screen.height + '  ratio: ' + window.devicePixelRatio); /* Lap: 1280x720; Mob: 432x896 */
+  //alert( 'Res: (W)' + screen.width + 'x(H)' + screen.height + '  ratio: ' + window.devicePixelRatio); /* Lap: 1280x720; Mob: 432x896 */
 
-  // ADD ROOT VARIABLE
-  document.documentElement.style.setProperty('--tpx', `calc(1px * ${window.devicePixelRatio})`);
   // COLORIZE PANELS
   let panels = document.getElementsByClassName("panel");
   let colors = ['car', 'cag', 'cab', 'cay', 'cat', 'cav'];
@@ -25,15 +24,22 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
+  // ADD ROOT VARIABLE
+  document.documentElement.style.setProperty('--tpx', `calc(1px * ${window.devicePixelRatio})`);
+
   //============================
   // SET BACKGROUND ICONS
   let icons = ["main-weapon", "sidearm", "mask", "backpack", "chest", "gloves", "holster", "kneepads"];
   for (let i = 0; i < icons.length; i++) {
-    let file = `./icons/${icons[i]}.png`;
-    let value = `linear-gradient(var(--cbgr75), var(--cbgr75)), url(${file}) center/contain no-repeat`;
-    let element = document.getElementsByClassName(`js_${icons[i]}`);
+    var img = new Image();
+    img.src = `./icons/${icons[i]}.png`;
+    let value = `linear-gradient(var(--cbgr75), var(--cbgr75)), url(${img.src}) center no-repeat`;
+    //let element = document.getElementsByClassName(`js_${icons[i]}`);
+    let element = document.querySelectorAll(`[id*="${icons[i]}"]`);
     for (let j = 0; j < element.length; j++) {
       element[j].style.background = value;
+      let scale = 0.75 * Math.min(element[j].offsetWidth/img.width, element[j].offsetHeight/img.height)
+      element[j].style.backgroundSize = `${img.width * scale}px ${img.height * scale}px`;
     }
   };
 
@@ -56,39 +62,73 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // item name
     let gearName = clonedListGearItem.querySelector('.item-name')
-    gearName.innerText = `${name}`;
-
-    // item color
-    let border = clonedListGearItem.querySelector('.border');
-    if (mask[`${name}`].rarity == "Exotic") {
-      border.style.borderColor = 'var(--cExotic)';
-      gearName.style.color = 'var(--cExotic)';
-    }
-    else if (mask[`${name}`].rarity == "Named") {
-      border.style.borderColor = 'var(--cNamed)';
-      gearName.style.color = 'var(--cNamed)';
-    }
-    else if (mask[`${name}`].rarity == "Set") {
-      border.style.borderColor = 'var(--cGearSet)';
-      gearName.style.color = 'var(--cGearSet)';
-    };
+    gearName.innerHTML = `${name}`;
 
     // item type
     if (mask[`${name}`].hasOwnProperty('type') && mask[`${name}`].type !== `${name}`) {
       let gearType = clonedListGearItem.querySelector('.item-type');
-      gearType.innerText = "(";
-      gearType.innerText += mask[`${name}`].type;
-      gearType.innerText += ")";
+      gearType.innerHTML = "(";
+      gearType.innerHTML += mask[`${name}`].type;
+      gearType.innerHTML += ")";
+    };
+
+    // item set boni
+    let statsSet = clonedListGearItem.querySelector('.stats-set');
+    statsSet.classList.add('hLineTop');
+    let setAttribute1 = clonedListGearItem.querySelector('.set-attribute1');
+    let setAttribute2 = clonedListGearItem.querySelector('.set-attribute2');
+    let setAttribute3 = clonedListGearItem.querySelector('.set-attribute3');
+    if (mask[`${name}`].rarity == "Exotic") {
+      setAttribute1.innerHTML = mask[`${name}`].attribute1Name + ': ';
+      setAttribute1.innerHTML += mask[`${name}`].attribute1Value;
+      setAttribute2.innerHTML = mask[`${name}`].attribute2Name + ': ';
+      setAttribute2.innerHTML += mask[`${name}`].attribute2Value;
+      setAttribute3.innerHTML = mask[`${name}`].attribute3Name + ': ';
+      setAttribute3.innerHTML += mask[`${name}`].attribute3Value;
+    }
+    else if (mask[`${name}`].rarity == "GearSet") {
+      let setName = mask[`${name}`].type;
+      setAttribute1.innerHTML = set[`${setName}`].attribute1Name + ': ';
+      setAttribute1.innerHTML += set[`${setName}`].attribute1Value;
+      setAttribute2.innerHTML = set[`${setName}`].attribute2Name + ': ';
+      setAttribute2.innerHTML += set[`${setName}`].attribute2Value;
+      setAttribute3.innerHTML = set[`${setName}`].gearSetTalentName + '<br><br>';
+      setAttribute3.innerHTML += set[`${setName}`].gearSetTalentText;
+    }
+    else if (mask[`${name}`].rarity !== "Improvised") {
+      let setName = mask[`${name}`].type;
+      console.log(setName);
+      setAttribute1.innerHTML = set[`${setName}`].attribute1Name + ': ';
+      setAttribute1.innerHTML += set[`${setName}`].attribute1Value;
+      setAttribute2.innerHTML = set[`${setName}`].attribute2Name + ': ';
+      setAttribute2.innerHTML += set[`${setName}`].attribute2Value;
+      setAttribute3.innerHTML = set[`${setName}`].attribute3Name + ': ';
+      setAttribute3.innerHTML += set[`${setName}`].attribute3Value;
     };
 
     // item talent
+    let gearTalentName = clonedListGearItem.querySelector('.item-talent-name')
     if (mask[`${name}`].hasOwnProperty('talentName')) {
-      let gearTalentName = clonedListGearItem.querySelector('.item-talent-name')
       gearTalentName.classList.add('hLineTop');
-      gearTalentName.innerText = mask[`${name}`].talentName;
+      gearTalentName.innerHTML = mask[`${name}`].talentName;
     };
     if (mask[`${name}`].hasOwnProperty('talentText')) {
-      clonedListGearItem.querySelector('.item-talent-text').innerText = mask[`${name}`].talentText;
+      clonedListGearItem.querySelector('.item-talent-text').innerHTML = mask[`${name}`].talentText;
+    };
+
+    // item color
+    let panelBorder = clonedListGearItem.querySelector('.panelBorder');
+    if (mask[`${name}`].rarity == "Exotic") {
+      panelBorder.style.borderColor = 'var(--cExotic)';
+      panelBorder.style.color = 'var(--cExotic)';
+    }
+    else if (mask[`${name}`].rarity == "Named") {
+      panelBorder.style.borderColor = 'var(--cNamed)';
+      panelBorder.style.color = 'var(--cNamed)';
+    }
+    else if (mask[`${name}`].rarity == "GearSet") {
+      panelBorder.style.borderColor = 'var(--cGearSet)';
+      panelBorder.style.color = 'var(--cGearSet)';
     };
 
     list.appendChild(clonedListGearItem);
---
+++ data/sets.js
@@ -0,0 +1,26 @@
+export const set = {
+  "Aces & Eights": {
+    attribute1Name: "Marksman Rifle Damage",
+    attribute1Value: 15,
+    attribute2Name: "Headshot Damage",
+    attribute2Value: 30,
+    gearSetTalentName: "Dead Man's Hand",
+    gearSetTalentText: "Flip a card when landing shots with a Marksman Rifle.<br> After 5 cards are flipped, the damage of your next shot is amplified by 30%. More shots are enhanced, the better the hand revealed.<br> <br> Four of a Kind: 4 shots.<br> Full House: 3 shots.<br> Aces and Eights: 2 shots.<br> <br> Flip an additional card on headshots."
+  },
+  "Gila Guard": {
+    attribute1Name: "Total Armor",
+    attribute1Value: 5,
+    attribute2Name: "Health",
+    attribute2Value: 10,
+    attribute3Name: "Armor Regeneration",
+    attribute3Value: 1,
+  },
+  "5.11 Tactical": {
+    attribute1Name: "Health",
+    attribute1Value: 10,
+    attribute2Name: "Incoming Repairs",
+    attribute2Value: 20,
+    attribute3Name: "Hazard Protection",
+    attribute3Value: 10,
+  }
+}
