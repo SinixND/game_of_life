f44ac9b57d83f5cbff61e0ed2571d34539f9daa8
SinixND <77335684+SinixND@users.noreply.github.com>
2023-02-15 16:29:02 +0100

    sync

--- data/sets.js
+++ data/sets.js
@@ -1,26 +1,26 @@
 export const sets = {
   "Aces & Eights": {
-    attribute1Name: "Marksman Rifle Damage",
-    attribute1Value: 15,
-    attribute2Name: "Headshot Damage",
-    attribute2Value: 30,
+    attributeCore: "Marksman Rifle Damage",
+    attributeCoreValue: 15,
+    attributeMinor1: "Headshot Damage",
+    attributeMinor1Value: 30,
     gearSetTalentName: "Dead Man's Hand",
     gearSetTalentText: "Flip a card when landing shots with a Marksman Rifle.<br> After 5 cards are flipped, the damage of your next shot is amplified by 30%. More shots are enhanced, the better the hand revealed.<br> <br> Four of a Kind: 4 shots.<br> Full House: 3 shots.<br> Aces and Eights: 2 shots.<br> <br> Flip an additional card on headshots."
   },
   "Gila Guard": {
-    attribute1Name: "Total Armor",
-    attribute1Value: 5,
-    attribute2Name: "Health",
-    attribute2Value: 10,
-    attribute3Name: "Armor Regeneration",
-    attribute3Value: 1,
+    attributeCore: "Total Armor",
+    attributeCoreValue: 5,
+    attributeMinor1: "Health",
+    attributeMinor1Value: 10,
+    attributeMinor2: "Armor Regeneration",
+    attributeMinor2Value: 1,
   },
   "5.11 Tactical": {
-    attribute1Name: "Health",
-    attribute1Value: 10,
-    attribute2Name: "Incoming Repairs",
-    attribute2Value: 20,
-    attribute3Name: "Hazard Protection",
-    attribute3Value: 10,
+    attributeCore: "Health",
+    attributeCoreValue: 10,
+    attributeMinor1: "Incoming Repairs",
+    attributeMinor1Value: 20,
+    attributeMinor2: "Hazard Protection",
+    attributeMinor2Value: 10,
   }
 }
