913e53dc0587ba4c42c10bad484287b50230901f
SinixND <77335684+SinixND@users.noreply.github.com>
2023-02-16 10:31:27 +0100

    update

--- data/attributes.js
+++ data/attributes.js
@@ -1,15 +1,21 @@
 export const attributes = {
-    "attributesArmor": {
-        "attributeCore": {
-            "Weapon Damage": "Weapon Damage",
-            "Weapon Damage Value": 15,
-            "Armor": "Armor",
-            "Armor Value": 170001,
-            "Skill Tier": "Skill Tier",
-            "Skill Tier Value": 1,
-        },
-        "attributeMinor": {
+  "attributesArmor": {
+    "attributeCore": {
+      "redCore": {
+        "name": "Weapon Damage",
+        "value": 15,
+      },
+      "blueCore": {
+        "Armor": "Armor",
+        "Armor Value": 170001,
+      },
+      "yellowCore": {
+        "Skill Tier": "Skill Tier",
+        "Skill Tier Value": 1,
+      }
+      },
+      "attributeMinor": {
 
-        },
+      },
     },
-}
+  }
