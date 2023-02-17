06a8b776518e6d9f5b53c8b6b5595b523f4c9610
SinixND <freak105@gmx.de>
2023-02-17 12:59:12 +0100

    update parametric dropdowns

--- data/attributes.js
+++ data/attributes.js
@@ -64,92 +64,92 @@ export const attributes = {
         value: 10,
       },
     },
-  },
-  mod: {
-    'Critical Hit Chance': {
-      png: "modRed",
-      value: 6,
-    },
-    'Critical Hit Damage': {
-      png: "modRed",
-      value: 12,
-    },
-    'Headshot Damage': {
-      png: "modRed",
-      value: 10,
-    },
-    'Armor on kill': {
-      png: "modBlue",
-      value: 18935,
-    },
-    'Bleed Resistance': {
-      png: "modBlue",
-      value: 10,
-    },
-    'Blind/Deaf Resistance': {
-      png: "modBlue",
-      value: 10,
-    },
-    'Burn Resistance': {
-      png: "modBlue",
-      value: 10,
-    },
-    'Disorient Resistance': {
-      png: "modBlue",
-      value: 10,
-    },
-    'Disrupt Resistance': {
-      png: "modBlue",
-      value: 10,
-    },
-    'Ensnare Resistance': {
-      png: "modBlue",
-      value: 10,
-    },
-    'Incomming Repairs': {
-      png: "modBlue",
-      value: 20,
-    },
-    'Pulse Resistance': {
-      png: "modBlue",
-      value: 10,
-    },
-    'Protection from Elites': {
-      png: "modBlue",
-      value: 13,
-    },
-    'Shock Resistance': {
-      png: "modBlue",
-      value: 10,
-    },
-    'Repair Skills': {
-      png: "modYellow",
-      value: 20,
-    },
-    'Skill Duration': {
-      png: "modYellow",
-      value: 10,
-    },
-    'Skill Haste': {
-      png: "modYellow",
-      value: 12,
-    },
-  },
-  talent: {
-    normal: {
-      'weapon': {
+    mod: {
+      'Critical Hit Chance': {
+        png: "modRed",
+        value: 6,
       },
-      'backpack': {
+      'Critical Hit Damage': {
+        png: "modRed",
+        value: 12,
       },
-      'chest': {
+      'Headshot Damage': {
+        png: "modRed",
+        value: 10,
+      },
+      'Armor on kill': {
+        png: "modBlue",
+        value: 18935,
+      },
+      'Bleed Resistance': {
+        png: "modBlue",
+        value: 10,
+      },
+      'Blind/Deaf Resistance': {
+        png: "modBlue",
+        value: 10,
+      },
+      'Burn Resistance': {
+        png: "modBlue",
+        value: 10,
+      },
+      'Disorient Resistance': {
+        png: "modBlue",
+        value: 10,
+      },
+      'Disrupt Resistance': {
+        png: "modBlue",
+        value: 10,
+      },
+      'Ensnare Resistance': {
+        png: "modBlue",
+        value: 10,
+      },
+      'Incomming Repairs': {
+        png: "modBlue",
+        value: 20,
+      },
+      'Pulse Resistance': {
+        png: "modBlue",
+        value: 10,
+      },
+      'Protection from Elites': {
+        png: "modBlue",
+        value: 13,
+      },
+      'Shock Resistance': {
+        png: "modBlue",
+        value: 10,
+      },
+      'Repair Skills': {
+        png: "modYellow",
+        value: 20,
+      },
+      'Skill Duration': {
+        png: "modYellow",
+        value: 10,
+      },
+      'Skill Haste': {
+        png: "modYellow",
+        value: 12,
       },
     },
-    named: {
-      'weapon': {
+    talent: {
+      normal: {
+        'weapon': {
+        },
+        'backpack': {
+        },
+        'chest': {
+        },
       },
-      'backpack': {
-      },
-      'chest': {
+      named: {
+        'weapon': {
+        },
+        'backpack': {
+        },
+        'chest': {
+        },
       },
     },
   },
