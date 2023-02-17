5b0b6b08f2012342e340ebe1eb9ea69a25383854
SinixND <77335684+SinixND@users.noreply.github.com>
2023-02-17 12:51:19 +0100

    update debug

--- code.js
+++ code.js
@@ -438,7 +438,7 @@ function applySelection(equipmentTypeName, gearTypeName, gearItem, gearItemName)
     const tplDropdownOptionBase = document.getElementById('template--dropdown--option');
     
     // iterate over attributes armor core
-    console.log(`attributeName: ${attributes[equipmentTypeName][attributeTypeNameData]}`)
+    console.log(`attributeName = ${Object.keys(attributes[equipmentTypeName][attributeTypeNameData])[0]}`)
     for (let attributeName of Object.keys(attributes[equipmentTypeName][attributeTypeNameData])) {
       let clonedDropdownOptionNode = tplDropdownOptionBase.content.cloneNode(true);
       let dropdownOption = clonedDropdownOptionNode.getElementById('dropdown--option-');
