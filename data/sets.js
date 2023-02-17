0413cd8391ac7feae8c6851da7d120f6ced8e258
SinixND <77335684+SinixND@users.noreply.github.com>
2023-02-17 15:31:17 +0100

    update sets structure

--- data/sets.js
+++ data/sets.js
@@ -1,52 +1,57 @@
 export const sets = {
-  "Aces & Eights": {
-    attribute1: {
-      name: "Marksman Rifle Damage",
-      requiredPieces: 2,
-      value: 15,
-    },
-    attribute2: {
-      name: "Headshot Damage",
-      requiredPieces: 3,
-      value: 30,
-    },
-    attribute3: {
-      name: "Dead Man's Hand",
-      requiredPieces: 4,
-      value: "Flip a card when landing shots with a Marksman Rifle.<br> After 5 cards are flipped, the damage of your next shot is amplified by 30%. More shots are enhanced, the better the hand revealed.<br> <br> Four of a Kind: 4 shots.<br> Full House: 3 shots.<br> Aces and Eights: 2 shots.<br> <br> Flip an additional card on headshots."
-  },
-  "Gila Guard": {
-    attribute1: {
-      name: "Total Armor",
-      requiredPieces: 1,
-      value: 5,
-    },
-    attribute2: {
-      name: "Health",
-      requiredPieces: 2,
-      value: 10,
-    },
-    attribute3: {
-      name: "Armor Regeneration",
-      requiredPieces: 3,
-      value: 1,
+  GearSet: {
+    "Aces & Eights": {
+      attribute1: {
+        name: "Marksman Rifle Damage",
+        requiredPieces: 2,
+        value: 15,
+      },
+      attribute2: {
+        name: "Headshot Damage",
+        requiredPieces: 3,
+        value: 30,
+      },
+      attribute3: {
+        name: "Dead Man's Hand",
+        requiredPieces: 4,
+        value: "Flip a card when landing shots with a Marksman Rifle.<br> After 5 cards are flipped, the damage of your next shot is amplified by 30%. More shots are enhanced, the better the hand revealed.<br> <br> Four of a Kind: 4 shots.<br> Full House: 3 shots.<br> Aces and Eights: 2 shots.<br> <br> Flip an additional card on headshots."
+      },
     },
   },
-  "5.11 Tactical": {
-    attribute1: {
-      name: "Health",
-      requiredPieces: 1,
-      value: 10,
+  BrandSet: {
+    "Gila Guard": {
+      attribute1: {
+        name: "Total Armor",
+        requiredPieces: 1,
+        value: 5,
+      },
+      attribute2: {
+        name: "Health",
+        requiredPieces: 2,
+        value: 10,
+      },
+      attribute3: {
+        name: "Armor Regeneration",
+        requiredPieces: 3,
+        value: 1,
+      },
     },
-    attribute2: {
-      name: "Incoming Repairs",
-      requiredPieces: 2,
-      value: 20,
+    "5.11 Tactical": {
+      attribute1: {
+        name: "Health",
+        requiredPieces: 1,
+        value: 10,
+      },
+      attribute2: {
+        name: "Incoming Repairs",
+        requiredPieces: 2,
+        value: 20,
+      },
+      attribute3: {
+        name: "Hazard Protection",
+        requiredPieces: 3,
+        value: 10,
+      },
     },
-    attribute3: {
-      name: "Hazard Protection",
-      requiredPieces: 3,
-      value: 10,
-    },
-  }
+  },
 }
