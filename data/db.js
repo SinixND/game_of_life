d033ac8c93286de65056faa712d10ea172e08ba3
SinixND <77335684+SinixND@users.noreply.github.com>
2023-03-06 12:50:04 +0100

    fixed typo in db.js

--- data/db.js
+++ data/db.js
@@ -213,7 +213,7 @@ export const db = {
           value: 30,
         },
         attribute3: {
-          name: "Dead Man"s Hand",
+          name: "Dead Man's Hand",
           requiredPieces: 4,
           value: "Flip a card when landing shots with a Marksman Rifle.<br> After 5 cards are flipped, the damage of your next shot is amplified by 30%. More shots are enhanced, the better the hand revealed.<br> <br> Four of a Kind: 4 shots.<br> Full House: 3 shots.<br> Aces and Eights: 2 shots.<br> <br> Flip an additional card on headshots."
         },
