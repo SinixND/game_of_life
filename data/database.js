b8538d7ee23154680816ef257d96320595b78802
676d <a.schaumberger@oth-aw.de>
2023-03-06 19:35:30 +0100

    test imported masks

--- data/database.js
+++ data/database.js
@@ -1,263 +1,508 @@
 export const db = {
-  "equipment": {
-    "armor": {
-      "mask": {
-        "Vile": {
+  'equipment': {
+    'armor': {
+      'mask': {
+        'Vile': {
           "rarity": "Exotic",
           "type": "Exotic",
+          'core': {
+            "name": "Skill Tier",
+          },
+          'minor 1': {
+            name: "Status Effects",
+          },
+          'minor 2': {
+            name: "Hazard Protection",
+          },
+          "talent": {
+            name: "Toxic Delivery",
+            value: "Status effects also apply a damage over time debuff for 10s.\r\n Total damage dealt is equal to 50% of your concussion grenade damage and increased by your status effect attributes.",
+          },
+        },
+        'Nightwatcher': {
+          "rarity": "Named",
+          "type": "Gila Guard",
+          'minor 1': {
+            name: "Scanner Pulse Haste",
+            value: 100,
+          },
+        },
+        'Aces & Eights': {
+          rarity: "GearSet",
+          type: "Aces & Eights",
+        },
+        '5.11 Tactical': {
+          rarity: "HighEnd",
+          type: "5.11 Tactical",
+        },
+        "Catharsis": {
+          "rarity": "Exotic",
+          "core": {
+            "name": "Armor",
+          },
+          "minor 1": {
+            "name": "Incoming Repairs",
+          },
+          "minor 2": {
+            "name": "Armor Regeneration",
+          },
+          "talent": {
+            "name": "Vicious Cycle",
+          }
+        },
+        "Coyote's Mask": {
+          "rarity": "Exotic",
+          "core": {
+            "name": "Weapon Damage",
+          },
+          "minor 1": {
+            "name": "Critical Hit Chance",
+          },
+          "minor 2": {
+            "name": "Critical Hit Damage",
+          },
+          "talent": {
+            "name": "Pack Instincts",
+          }
+        },
+        "Airaldi Holdings": {
+          "rarity": "HighEnd",
+          "type": "Airaldi Holdings",
+          "core": {
+            "name": "Weapon Damage",
+          },
+        },
+        "Alps Summit Armaments": {
+          "rarity": "HighEnd",
+          "type": "Alps Summit Armaments",
           "core": {
             "name": "Skill Tier",
           },
-          "minor 1": {
-            "name": "Status Effects",
-          },
-          "minor 2": {
-            "name": "Hazard Protection",
-          },
-          "talent": {
-            "name": "Toxic Delivery",
-            "value": "Status effects also apply a damage over time debuff for 10s.\r\n Total damage dealt is equal to 50% of your concussion grenade damage and increased by your status effect attributes.",
-          },
         },
-        "Nightwatcher": {
-          "rarity": "Named",
-          "type": "Gila Guard",
-          "minor 1": {
-            "name": "Scanner Pulse Haste",
-            "png": "minorYellow",
-            "value": 100,
-          },
-        },
-        "Aces & Eights": {
-          "rarity": "Gearset",
-          "type": "Aces & Eights",
-        },
-        "5.11 Tactical": {
+        "Badger Tuff": {
           "rarity": "HighEnd",
-          "type": "5.11 Tactical",
+          "type": "Badger Tuff",
+          "core": {
+            "name": "Armor",
+          },
+        },
+        "Ceska Vyroba s.r.o.": {
+          "rarity": "HighEnd",
+          "type": "Ceska Vyroba s.r.o.",
+          "core": {
+            "name": "Weapon Damage",
+          },
+        },
+        "China Light Industries Corporation": {
+          "rarity": "HighEnd",
+          "type": "China Light Industries Corporation",
+          "core": {
+            "name": "Skill Tier",
+          },
+        },
+        "Douglas & Harding": {
+          "rarity": "HighEnd",
+          "type": "Douglas & Harding",
+          "core": {
+            "name": "Weapon Damage",
+          },
+        },
+        "Punch Drunk": {
+          "rarity": "Named",
+          "type": "Douglas & Harding",
+          "core": {
+            "name": "Weapon Damage",
+          },
+          "Minor": "Headshot Damage",
+        },
+        "Fenris Group AB": {
+          "rarity": "HighEnd",
+          "type": "Fenris Group AB",
+          "core": {
+            "name": "Weapon Damage",
+          },
+        },
+        "Foundry Bullwark": {
+          "rarity": "Gearset",
+          "type": "Foundry Bullwark",
+        },
+        "Future Initiative": {
+          "rarity": "Gearset",
+          "type": "Future Initiative",
+        },
+        "Gila Guard": {
+          "rarity": "HighEnd",
+          "type": "Gila Guard",
+          "core": {
+            "name": "Armor",
+          },
+        },
+        "Golan Gear Ltd": {
+          "rarity": "HighEnd",
+          "type": "Golan Gear Ltd",
+          "core": {
+            "name": "Armor",
+          },
+        },
+        "Grupo Sombra S.A.": {
+          "rarity": "HighEnd",
+          "type": "Grupo Sombra S.A.",
+          "core": {
+            "name": "Weapon Damage",
+          },
+        },
+        "Hana-U Corporation": {
+          "rarity": "HighEnd",
+          "type": "Hana-U Corporation",
+          "core": {
+            "name": "Skill Tier",
+          },
+        },
+        "Hard Wired": {
+          "rarity": "Gearset",
+          "type": "Hard Wired",
+          "core": {
+            "name": "Skill Tier",
+          },
+        },
+        "Murakami Industries": {
+          "rarity": "HighEnd",
+          "type": "Murakami Industries",
+          "core": {
+            "name": "Skill Tier",
+          },
+        },
+        "Negotiator's Dilemma": {
+          "rarity": "Gearset",
+          "type": "Negotiator's Dilemma",
+          "core": {
+            "name": "Weapon Damage",
+          },
+        },
+        "Ongoing Directive": {
+          "rarity": "Gearset",
+          "type": "Ongoing Directive",
+          "core": {
+            "name": "Skill Tier",
+          },
+        },
+        "Overlord Armaments": {
+          "rarity": "HighEnd",
+          "type": "Overlord Armaments",
+          "core": {
+            "name": "Weapon Damage",
+          },
+        },
+        "Petrov Defense Group": {
+          "rarity": "HighEnd",
+          "type": "Petrov Defense Group",
+          "core": {
+            "name": "Weapon Damage",
+          },
+        },
+        "Providence Defense": {
+          "rarity": "HighEnd",
+          "type": "Providence Defense",
+          "core": {
+            "name": "Weapon Damage",
+          },
+        },
+        "Richter & Kaiser GmbH": {
+          "rarity": "HighEnd",
+          "type": "Richter & Kaiser GmbH",
+          "core": {
+            "name": "Skill Tier",
+          },
+        },
+        "Sokolov Concern": {
+          "rarity": "HighEnd",
+          "type": "Sokolov Concern",
+          "core": {
+            "name": "Weapon Damage",
+          },
+        },
+        "Striker's Battlegear": {
+          "rarity": "Gearset",
+          "type": "Striker's Battlegear",
+          "core": {
+            "name": "Weapon Damage",
+          },
+        },
+        "System Corruption": {
+          "rarity": "Gearset",
+          "type": "System Corruption",
+          "core": {
+            "name": "Weapon Damage",
+          },
+        },
+        "Tip of the Spear": {
+          "rarity": "Gearset",
+          "type": "Tip of the Spear",
+          "core": {
+            "name": "Weapon Damage",
+          },
+        },
+        "True Patriot": {
+          "rarity": "Gearset",
+          "type": "True Patriot",
+          "core": {
+            "name": "Armor",
+          },
+        },
+        "Wyvern Wear": {
+          "rarity": "HighEnd",
+          "type": "Wyvern Wear",
+          "core": {
+            "name": "Skill Tier",
+          },
+        },
+        "Yaahl Gear": {
+          "rarity": "HighEnd",
+          "type": "Yaahl Gear",
+          "core": {
+            "name": "Armor",
+          },
+        },
+        "The Hollow Man": {
+          "rarity": "Named",
+          "type": "Yaahl Gear",
+          "core": {
+            "name": "Armor",
+          },
+          "minor 1": {
+            "name": "Damage to Health",
+          },
         },
       },
     },
   },
-  "attribute": {
-    "armor": {
-      "core": {
-        "Weapon Damage": {
-          "png": "coreRed",
-          "value": 15,
+  'attribute': {
+    armor: {
+      'core': {
+        'Weapon Damage': {
+          png: "coreRed",
+          value: 15,
         },
-        "Armor": {
-          "png": "coreBlue",
-          "value": 170001,
+        'Armor': {
+          png: "coreBlue",
+          value: 170001,
         },
-        "Skill Tier": {
-          "png": "coreYellow",
-          "value": 1,
+        'Skill Tier': {
+          png: "coreYellow",
+          value: 1,
         },
       },
-      "minor": {
-          "Critical Hit Chance": {
-            "png": "minorRed",
-            "value": 6,
+      'minor': {
+        "HighEnd": {
+          'Critical Hit Chance': {
+            png: "minorRed",
+            value: 6,
           },
-          "Critical Hit Damage": {
-            "png": "minorRed",
-            "value": 12,
+          'Critical Hit Damage': {
+            png: "minorRed",
+            value: 12,
           },
-          "Headshot Damage": {
-            "png": "minorRed",
-            "value": 10,
+          'Headshot Damage': {
+            png: "minorRed",
+            value: 10,
           },
-          "Weapon Handling": {
-            "png": "minorRed",
-            "value": 8,
+          'Weapon Handling': {
+            png: "minorRed",
+            value: 8,
           },
-          "Armor Regeneration": {
-            "png": "minorBlue",
-            "value": 4925,
+          'Armor Regeneration': {
+            png: "minorBlue",
+            value: 4925,
           },
-          "Explosive Resistance": {
-            "png": "minorBlue",
-            "value": 10,
+          'Explosive Resistance': {
+            png: "minorBlue",
+            value: 10,
           },
-          "Hazard Protection": {
-            "png": "minorBlue",
-            "value": 10,
+          'Hazard Protection': {
+            png: "minorBlue",
+            value: 10,
           },
-          "Health": {
-            "png": "minorBlue",
-            "value": 18935,
+          'Health': {
+            png: "minorBlue",
+            value: 18935,
           },
-          "Repair Skills": {
-            "png": "minorYellow",
-            "value": 20,
+          'Repair Skills': {
+            png: "minorYellow",
+            value: 20,
           },
-          "Skill Damage": {
-            "png": "minorYellow",
-            "value": 10,
+          'Skill Damage': {
+            png: "minorYellow",
+            value: 10,
           },
-          "Skill Haste": {
-            "png": "minorYellow",
-            "value": 12,
+          'Skill Haste': {
+            png: "minorYellow",
+            value: 12,
           },
-          "Status Effects": {
-            "png": "minorYellow",
-            "value": 10,
+          'Status Effects': {
+            png: "minorYellow",
+            value: 10,
           },
+        },
+        "Named": {
+          'Scanner Pulse Haste': {
+            png: "minorYellow",
+            value: 100,
+          }
+        },
+      },
+      "minor named": {
+        'Scanner Pulse Haste': {
+          png: "minorYellow",
+          value: 100,
+        }
       },
     },
   },
-  "mod": {
-    "Critical Hit Chance": {
-      "png": "modRed",
-      "value": 6,
+  'mod': {
+    'Critical Hit Chance': {
+      png: "modRed",
+      value: 6,
     },
-    "Critical Hit Damage": {
-      "png": "modRed",
-      "value": 12,
+    'Critical Hit Damage': {
+      png: "modRed",
+      value: 12,
     },
-    "Headshot Damage": {
-      "png": "modRed",
-      "value": 10,
+    'Headshot Damage': {
+      png: "modRed",
+      value: 10,
     },
-    "Armor on kill": {
-      "png": "modBlue",
-      "value": 18935,
+    'Armor on kill': {
+      png: "modBlue",
+      value: 18935,
     },
-    "Bleed Resistance": {
-      "png": "modBlue",
-      "value": 10,
+    'Bleed Resistance': {
+      png: "modBlue",
+      value: 10,
     },
-    "Blind/Deaf Resistance": {
-      "png": "modBlue",
-      "value": 10,
+    'Blind/Deaf Resistance': {
+      png: "modBlue",
+      value: 10,
     },
-    "Burn Resistance": {
-      "png": "modBlue",
-      "value": 10,
+    'Burn Resistance': {
+      png: "modBlue",
+      value: 10,
     },
-    "Disorient Resistance": {
-      "png": "modBlue",
-      "value": 10,
+    'Disorient Resistance': {
+      png: "modBlue",
+      value: 10,
     },
-    "Disrupt Resistance": {
-      "png": "modBlue",
-      "value": 10,
+    'Disrupt Resistance': {
+      png: "modBlue",
+      value: 10,
     },
-    "Ensnare Resistance": {
-      "png": "modBlue",
-      "value": 10,
+    'Ensnare Resistance': {
+      png: "modBlue",
+      value: 10,
     },
-    "Incomming Repairs": {
-      "png": "modBlue",
-      "value": 20,
+    'Incomming Repairs': {
+      png: "modBlue",
+      value: 20,
     },
-    "Pulse Resistance": {
-      "png": "modBlue",
-      "value": 10,
+    'Pulse Resistance': {
+      png: "modBlue",
+      value: 10,
     },
-    "Protection from Elites": {
-      "png": "modBlue",
-      "value": 13,
+    'Protection from Elites': {
+      png: "modBlue",
+      value: 13,
     },
-    "Shock Resistance": {
-      "png": "modBlue",
-      "value": 10,
+    'Shock Resistance': {
+      png: "modBlue",
+      value: 10,
     },
-    "Repair Skills": {
-      "png": "modYellow",
-      "value": 20,
+    'Repair Skills': {
+      png: "modYellow",
+      value: 20,
     },
-    "Skill Duration": {
-      "png": "modYellow",
-      "value": 10,
+    'Skill Duration': {
+      png: "modYellow",
+      value: 10,
     },
-    "Skill Haste": {
-      "png": "modYellow",
-      "value": 12,
+    'Skill Haste': {
+      png: "modYellow",
+      value: 12,
     },
   },
-  "talent": {
-    "weapon": {
-      "primary": {
-      },
-      "sidearm": {
-      },
+  'talent': {
+    'weapon': {
     },
-    "armor": {
-      "backpack": {
-      },
-      "chest": {
-      },
-    }
+    'backpack': {
+    },
+    'chest': {
+    },
   },
-  "set": {
-    "GearSet": {
+  'set': {
+    GearSet: {
       "Marksman Rifle Damage": 15,
     },
-    "BrandSet": {
+    BrandSet: {
 
     },
   },
-  "setboni": {
-    "GearSet": {
+  'setboni': {
+    GearSet: {
       "Aces & Eights": {
-        "attribute1": {
-          "name": "Marksman Rifle Damage",
-          "requiredPieces": 2,
-          "value": 15,
+        attribute1: {
+          name: "Marksman Rifle Damage",
+          requiredPieces: 2,
+          value: 15,
         },
-        "attribute2": {
-          "name": "Headshot Damage",
-          "requiredPieces": 3,
-          "value": 30,
+        attribute2: {
+          name: "Headshot Damage",
+          requiredPieces: 3,
+          value: 30,
         },
-        "attribute3": {
-          "name": "Dead Man's Hand",
-          "requiredPieces": 4,
-          "value": "Flip a card when landing shots with a Marksman Rifle.<br> After 5 cards are flipped, the damage of your next shot is amplified by 30%. More shots are enhanced, the better the hand revealed.<br> <br> Four of a Kind: 4 shots.<br> Full House: 3 shots.<br> Aces and Eights: 2 shots.<br> <br> Flip an additional card on headshots."
+        attribute3: {
+          name: "Dead Man's Hand",
+          requiredPieces: 4,
+          value: "Flip a card when landing shots with a Marksman Rifle.<br> After 5 cards are flipped, the damage of your next shot is amplified by 30%. More shots are enhanced, the better the hand revealed.<br> <br> Four of a Kind: 4 shots.<br> Full House: 3 shots.<br> Aces and Eights: 2 shots.<br> <br> Flip an additional card on headshots."
         },
       },
     },
-    "BrandSet": {
+    BrandSet: {
       "Gila Guard": {
-        "attribute1": {
-          "name": "Total Armor",
-          "requiredPieces": 1,
-          "value": 5,
+        attribute1: {
+          name: "Total Armor",
+          requiredPieces: 1,
+          value: 5,
         },
-        "attribute2": {
-          "name": "Health",
-          "requiredPieces": 2,
-          "value": 10,
+        attribute2: {
+          name: "Health",
+          requiredPieces: 2,
+          value: 10,
         },
-        "attribute3": {
-          "name": "Armor Regeneration",
-          "requiredPieces": 3,
-          "value": 1,
+        attribute3: {
+          name: "Armor Regeneration",
+          requiredPieces: 3,
+          value: 1,
         },
       },
       "5.11 Tactical": {
-        "attribute1": {
-          "name": "Health",
-          "requiredPieces": 1,
-          "value": 10,
+        attribute1: {
+          name: "Health",
+          requiredPieces: 1,
+          value: 10,
         },
-        "attribute2": {
-          "name": "Incoming Repairs",
-          "requiredPieces": 2,
-          "value": 20,
+        attribute2: {
+          name: "Incoming Repairs",
+          requiredPieces: 2,
+          value: 20,
         },
-        "attribute3": {
-          "name": "Hazard Protection",
-          "requiredPieces": 3,
-          "value": 10,
+        attribute3: {
+          name: "Hazard Protection",
+          requiredPieces: 3,
+          value: 10,
         },
       },
     },
   },
   /*
-  "weapons": {
+  'weapons': {
 
   },
   */
