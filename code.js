bbde632cb9442463f504fbc111137e73e910258d
SinixND <77335684+SinixND@users.noreply.github.com>
2023-03-07 19:11:18 +0100

    update

--- code.js
+++ code.js
@@ -235,8 +235,7 @@ for (let equipmentTypeName in db['equipment']) {
         if (gearItem.rarity == "Gearset") {
           for (let attribute in mltpc.Gearset[mltpcName]) {
             entryMltpcAttributes.innerHTML += "<li>" + mltpc.Gearset[mltpcName][attribute].name + ': ';
-            console.log(mltpc.Gearset[mltpcName][attribute])
-            if (mltpc.Gearset[mltpcName][attribute].constructor.name == 'pcs4') {entryMltpcAttributes.innerHTML += "\r\n"};
+            if (mltpc.Gearset[mltpcName][attribute].requiredPieces == 4) {entryMltpcAttributes.innerHTML += "\r\n"};
             entryMltpcAttributes.innerHTML += mltpc.Gearset[mltpcName][attribute].value + "</li>";
           }
         }
