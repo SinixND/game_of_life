a19beed187163f152e79c6bd79ca0dee77e000be
SinixND <77335684+SinixND@users.noreply.github.com>
2023-03-10 12:41:13 +0100

    update memento talent line breaks

--- data/database.js
+++ data/database.js
@@ -429,7 +429,7 @@ export const db = {
           },
           talent: {
             name: "Kill Confirmed",
-            value: "Enemies you kill drop a trophy on death. Collecting trophies provides both a short and long-term buff, the first of which scales with the number of core attributes equipped and lasts 10 seconds.\n\r \n\r 5% weapon damage per red core,\n\r 10% bonus armor per blue core.\n\r 5% skill efficiency per yellow core.\n\r \n\r For every trophy collected, gain an additional +1% weapon damage, +1% skill efficiency, and +0.1% armor regeneration for 300s. Maximum 30 stacks."
+            value: "Enemies you kill drop a trophy on death. Collecting trophies provides both a short and long-term buff, the first of which scales with the number of core attributes equipped and lasts 10 seconds.\n \n 5% weapon damage per red core,\n 10% bonus armor per blue core.\n 5% skill efficiency per yellow core.\n \n For every trophy collected, gain an additional +1% weapon damage, +1% skill efficiency, and +0.1% armor regeneration for 300s. Maximum 30 stacks."
           },
         },
         "NinjaBike Messenger Bag": {
