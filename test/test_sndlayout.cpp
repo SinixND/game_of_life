edf9a40443caa6d07af3bcbcbf65da50bbbfbb3d
SinixND <77335684+SinixND@users.noreply.github.com>
2023-08-31 14:19:42 +0200

    update test utility

--- test/test_sndlayout.cpp
+++ test/test_sndlayout.cpp
@@ -1,2 +1,10 @@
-#include <stdio.h>
+#include "catch2/catch_all.hpp"
+
 #include "sndlayout.h"
+
+TEST_CASE("Test catch2", "[tag]") {
+    REQUIRE(true);
+    REQUIRE(false);
+}
+
+git clone https://github.com/catchorg/Catch2/tree/devel/src/catch2
